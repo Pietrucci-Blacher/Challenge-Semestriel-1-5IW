ac78c65b951ac277a7aab28791d7e9a2d38d9f2c
Ossama9 <oussama.dahbi98@gmail.com>
2023-10-14 01:41:53 +0200

    Home ok

---
+++ front/src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Index(){
+    return (
+        <>
+        <div>
+            home
+
+            <br />
+            <Link href="/auth/login">Login</Link>
+
+            <br />
+            <br />
+            <Link href="/auth/register">Register</Link>
+        </div>
+        </>
+    )
+}
