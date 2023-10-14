475fb859b41d9d0fa0bac5f02ab9af9203bb0dc7
teddy <teddy.gauthier@outlook.com>
2023-10-14 22:39:39 +0200

    [fix] ApiRessource of entity

--- back/api/src/Entity/Schedule.php
+++ back/api/src/Entity/Schedule.php
@@ -4,7 +4,23 @@ namespace App\Entity;
 
 use App\Repository\ScheduleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
 
+#[ApiResource(
+    mercure: true,
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Get(),
+        new Delete(),
+        new Patch(),
+    ],
+)]
 #[ORM\Entity(repositoryClass: ScheduleRepository::class)]
 class Schedule
 {
--- back/api/src/Entity/Service.php
+++ back/api/src/Entity/Service.php
@@ -7,7 +7,23 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
 
+#[ApiResource(
+    mercure: true,
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Get(),
+        new Delete(),
+        new Patch(),
+    ],
+)]
 #[ORM\Entity(repositoryClass: ServiceRepository::class)]
 class Service
 {
