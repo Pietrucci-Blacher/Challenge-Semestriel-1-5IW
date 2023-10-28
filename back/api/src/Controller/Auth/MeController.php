fd0d67af125afa63481534317cd104276930fa99
Ossama9 <oussama.dahbi98@gmail.com>
2023-10-29 01:07:30 +0200

    update profile ok

--- back/api/src/Controller/Auth/MeController.php
+++ back/api/src/Controller/Auth/MeController.php
@@ -5,19 +5,34 @@ namespace App\Controller\Auth;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 #[AsController]
 class MeController  extends AbstractController
 {
+    private SerializerInterface $serializer;
+    private Security $security;
+
+    public function __construct(Security $security, SerializerInterface $serializer)
+    {
+        $this->security = $security;
+        $this->serializer = $serializer;
+    }
 
 
-    public function __invoke()
+    public function __invoke(): Response
     {
         /** @var User $user */
         $user = $this->getUser();
 
-        return $user;
+        if (!$user) {
+            return new Response(null, Response::HTTP_FORBIDDEN);
+        }
+        $data = $this->serializer->serialize($user, 'json', ['groups' => ['auth:me']]);
+
+        return new Response($data, Response::HTTP_OK, ['Content-Type' => 'application/json']);
     }
 }
