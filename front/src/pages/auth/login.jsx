a19d6d17f1ee5c5ffbf7f9ef66bd960b68660695
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-11-18 23:51:13 +0100

    Feat: Add translations

--- front/src/pages/auth/login.jsx
+++ front/src/pages/auth/login.jsx
@@ -66,18 +66,18 @@ export default function Login() {
             <br/>
             <br/>
 
-            <br/>
-            <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmitLogin}>
-                <div>
-                    <Input label="Email" type="email" placeholder="Email" onChange={handleInputEmailChange}
-                           value={formData.email}/>
-                </div>
-                <div>
-                    <Input label="Password" type="Password" placeholder="Password"
-                           onChange={handleInputPasswordChange} value={formData.password}/>
-                </div>
-                <GenericButton label="Login"/>
-            </form>
+                <br/>
+                <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmitLogin}>
+                    <div>
+                        <Input label="Email" type="email" placeholder="Email" onChange={handleInputEmailChange}
+                               value={formData.email}/>
+                    </div>
+                    <div>
+                        <Input label="Password" type="Password" placeholder="Password"
+                               onChange={handleInputPasswordChange} value={formData.password}/>
+                    </div>
+                    <GenericButton label="Login"/>
+                </form>
 
             <br/>
             <Link href="/auth/register">Register</Link>
