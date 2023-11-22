6320a6da46b5a4da52800a5348f4cbb99b6d5314
teddy <teddy.gauthier@outlook.com>
2023-11-23 00:57:00 +0100

    feat(establishment) : add several page

--- front/src/pages/provider/establishment/[id].jsx
+++ front/src/pages/provider/establishment/[id].jsx
@@ -1,28 +1,54 @@
 import { useRouter } from "next/router";
 import { useEstablishment } from "@/hooks/useEstablishment";
 import { useEffect } from "react";
+import { Button as FlowbiteButton } from "flowbite-react";
+import Link from "next/link";
+import GenericButton from "@/components/GenericButton";
+import { deleteEstablishment } from "@/services/establishmentService";
+import { useToast } from "@/hooks/useToast";
 
 export default function ShowEstablishment() {
     const router = useRouter();
     const { id } = router.query;
-    const { establishment, getEstablishment } = useEstablishment();
+    const { establishment, getEstablishmentById } = useEstablishment();
 
     useEffect(() => {
-        getEstablishment(id);
-    });
+        getEstablishmentById(id);
+    }, [id]);
+
+    const handleDelete = async (event) => {
+        try {
+            await deleteEstablishment(id);
+            router.push("/provider/establishment");
+        } catch (error) {
+            createToastMessage("error", "Une erreur est survenue");
+        }
+    };
+
+    const renderEstablishment = establishment ? (
+        <div>
+            <h1>{establishment.name}</h1>
+            <p>{establishment.street}</p>
+            <p>{establishment.city}</p>
+            <p>{establishment.zipCode}</p>
+        </div>
+    ) : (
+        "Chargement..."
+    );
 
     return (
         <>
-            {establishment ? (
-                <div>
-                    <h1>{establishment.name}</h1>
-                    <p>{establishment.street}</p>
-                    <p>{establishment.city}</p>
-                    <p>{establishment.zipCode}</p>
-                </div>
-            ) : (
-                "Chargement..."
-            )}
+            <div>{renderEstablishment}</div>
+            <GenericButton onClick={handleDelete} label="Supprimer" />
+            <FlowbiteButton
+                as={Link}
+                href={`/provider/establishment/update/${id}`}
+            >
+                Modifier
+            </FlowbiteButton>
+            <FlowbiteButton as={Link} href="/provider/establishment">
+                Retour
+            </FlowbiteButton>
         </>
     );
 }
--- front/src/pages/provider/establishment/index.jsx
+++ front/src/pages/provider/establishment/index.jsx
@@ -1,112 +1,52 @@
-import GenericButton from "@/components/GenericButton";
-import Input from "@/components/Input";
-import { useEffect, useState } from "react";
-import { useToast } from "@/hooks/useToast";
 import { useEstablishment } from "@/hooks/useEstablishment";
-import { useRouter } from "next/router";
-import { createEstablishment } from "@/services/establishmentService";
+import { useEffect } from "react";
+import { Button as FlowbiteButton } from "flowbite-react";
+import Link from "next/link";
 
-export default function CreateEstablishment() {
-    const { createToastMessage } = useToast();
-    const { create } = useEstablishment();
-    const router = useRouter();
+export default function ListEstablishment() {
+    const { establishments, getMyEstablishments } = useEstablishment();
 
-    const [formData, setFormData] = useState({
-        name: "",
-        street: "",
-        city: "",
-        zipCode: "",
-    });
+    useEffect(() => {
+        getMyEstablishments();
+    }, []);
 
-    const handleInputNameChange = (value) => {
-        setFormData({ ...formData, name: value });
-    };
-
-    const handleInputStreetChange = (value) => {
-        setFormData({ ...formData, street: value });
-    };
-
-    const handleInputCityChange = (value) => {
-        setFormData({ ...formData, city: value });
-    };
-
-    const handleInputZipCodeChange = (value) => {
-        setFormData({ ...formData, zipCode: value });
-    };
-
-    const handleSubmitCreate = async (event) => {
-        event.preventDefault();
-        const { name, street, city, zipCode } = formData;
-
-        if (!name || !street || !city || !zipCode) {
-            createToastMessage("error", "Veuillez remplir tous les champs");
-            return;
-        }
-
-        try {
-            const establishment = await createEstablishment({
-                name,
-                street,
-                city,
-                zipCode,
-            });
-
-            if (!establishment) {
-                createToastMessage("error", "Une erreur est survenue");
-                return;
-            }
-
-            router.push(`/provider/establishment/${establishment.id}`);
-        } catch (error) {
-            createToastMessage("error", error);
-        }
-    };
+    const renderEstablishments = establishments
+        ? establishments.map((establishment) => (
+              <tr key={establishment.id}>
+                  <td>{establishment.name}</td>
+                  <td>{establishment.street}</td>
+                  <td>{establishment.city}</td>
+                  <td>{establishment.zipCode}</td>
+                  <td>
+                      <FlowbiteButton
+                          as={Link}
+                          href={`/provider/establishment/${establishment.id}`}
+                      >
+                          Voir
+                      </FlowbiteButton>
+                  </td>
+              </tr>
+          ))
+        : "Chargement en cours";
 
     return (
-        <div>
-            <h1>Create</h1>
-            <form
-                className="flex max-w-md flex-col gap-4"
-                onSubmit={handleSubmitCreate}
-            >
-                <div>
-                    <Input
-                        label="Nom"
-                        type="text"
-                        placeholder="Entrer un nom"
-                        value={formData.name}
-                        onChange={handleInputNameChange}
-                    />
-                </div>
-                <div>
-                    <Input
-                        label="Rue"
-                        type="text"
-                        placeholder="Entrer une rue"
-                        value={formData.street}
-                        onChange={handleInputStreetChange}
-                    />
-                </div>
-                <div>
-                    <Input
-                        label="Ville"
-                        type="text"
-                        placeholder="Entrer une ville"
-                        value={formData.city}
-                        onChange={handleInputCityChange}
-                    />
-                </div>
-                <div>
-                    <Input
-                        label="Code postal"
-                        type="text"
-                        placeholder="Entrer un code postal"
-                        value={formData.zipCode}
-                        onChange={handleInputZipCodeChange}
-                    />
-                </div>
-                <GenericButton label="Creer un etablisement" />
-            </form>
-        </div>
+        <>
+            <h2>ListEstablishment</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Rue</th>
+                        <th>Ville</th>
+                        <th>Code postal</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>{renderEstablishments}</tbody>
+            </table>
+            <FlowbiteButton as={Link} href="/provider/establishment/create">
+                Créer un établissement
+            </FlowbiteButton>
+        </>
     );
 }
