054ae779fae9e983e414c06f14759eb452a658c0
Ossama9 <oussama.dahbi98@gmail.com>
2023-11-26 19:11:55 +0100

    add team member entity

---
+++ back/api/src/Entity/TeamMember.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use App\Controller\Provider\InviteTeamMemberController;
+use App\Dto\InviteTeamMemberDto;
+use App\Repository\TeamMemberRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: TeamMemberRepository::class)]
+#[ApiResource(
+    operations: [
+        new Post(
+            controller: InviteTeamMemberController::class,
+            input: InviteTeamMemberDto::class,
+            name: 'invite_team_member'
+        ),
+        new Get(
+            security: 'is_granted("ROLE_ADMIN") or (object.getMember() == user) or (object.getEtablishement().getOwner() == user)',
+            securityMessage: 'Access interdit.',
+        ),
+        new Patch(
+            denormalizationContext: ['groups' => ['team_member:update']],
+            security: 'object.getMember() == user',
+            securityMessage: 'Access interdit.'
+        )
+    ]
+)]
+class TeamMember
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'teamMembers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Establishment $etablishement = null;
+
+    #[ORM\ManyToOne(inversedBy: 'teamMembers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $member = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(["team_member:update"])]
+    #[Assert\Choice(choices: ['pending', 'approved', 'rejected'], message: 'Invalid status')]
+    private ?string $status = 'pending';
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEtablishement(): ?Establishment
+    {
+        return $this->etablishement;
+    }
+
+    public function setEtablishement(?Establishment $etablishement): static
+    {
+        $this->etablishement = $etablishement;
+
+        return $this;
+    }
+
+    public function getMember(): ?User
+    {
+        return $this->member;
+    }
+
+    public function setMember(?User $member): static
+    {
+        $this->member = $member;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
