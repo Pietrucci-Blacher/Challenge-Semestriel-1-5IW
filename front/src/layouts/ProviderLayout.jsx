4605b0b8b57f679d3971c1856be7a4f1731d3e78
Ossama9 <oussama.dahbi98@gmail.com>
2023-12-06 18:32:06 +0100

    layout ok

--- front/src/layouts/ProviderLayout.jsx
+++ front/src/layouts/ProviderLayout.jsx
@@ -1,11 +1,12 @@
 import Sidebar from "@/components/Sidebar";
-import {HiChartPie, HiInbox, HiShoppingBag, HiTable} from "react-icons/hi";
+import {HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser} from "react-icons/hi";
 
 const sidebarContent = [
     {icon: HiChartPie, text: "Dashboard", href: "#", label: "Pro", labelColor: "gray"},
-    {icon: HiInbox, text: "My Establishment", href: "#", label: "Pro", labelColor: "gray"},
+    {icon: HiInbox, text: "My Establishment", href: "/provider/establishment", label: "Pro", labelColor: "gray"},
     {icon: HiShoppingBag, text: "My Services", href: "#", label: "Pro", labelColor: "gray"},
-    {icon: HiTable, text: "My Reviews", href: "#", label: "Pro", labelColor: "gray"}
+    {icon: HiTable, text: "My Reviews", href: "#", label: "Pro", labelColor: "gray"},
+    {icon: HiUser, text: "My Teams", href: "/provider/establishment/team", label: "Pro", labelColor: "gray"},
 ]
 const ProviderLayout = ({children}) => {
     return (
