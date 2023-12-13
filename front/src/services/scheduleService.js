508de5ce43db1551f81425b3d13fc1b7d4f45d24
Ossama9 <oussama.dahbi98@gmail.com>
2023-12-13 20:24:44 +0100

    scheduleService.js updated

--- front/src/services/scheduleService.js
+++ front/src/services/scheduleService.js
@@ -1,11 +1,19 @@
 import httpClient from "./httpClient";
 
-export const addScheduleService = ({ reason, startTime, endTime}) => {
-    return httpClient.post(`/schedules`, { reason, startTime, endTime});
+export const addScheduleService = ({ reason, startTime, endTime, establishment}) => {
+    return httpClient.post(`/schedules`, { reason, startTime, endTime, establishment});
 };
 
 export const getUserSchedulesService = ({ userId }) => {
     return httpClient.get(`users/${userId}/schedules`);
+};
+
+export const getEstablishmentSchedulesService = ({ establishmentId }) => {
+    return httpClient.get(`establishments/${establishmentId}/schedules`);
+};
+
+export const getSchedulesByUserAndEstablishmentService = ({ establishmentId,userId }) => {
+    return httpClient.get(`establishments/${establishmentId}/users/${userId}/schedules`);
 };
 
 export const updateScheduleService = (id,payload) => {
