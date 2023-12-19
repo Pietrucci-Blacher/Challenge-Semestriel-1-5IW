7b61344aea680a43500c8bd203092d5be56a2de5
teddy <teddy.gauthier@outlook.com>
2023-12-19 01:06:10 +0100

    fix(utils) : minor fix

--- front/src/utils/utils.js
+++ front/src/utils/utils.js
@@ -8,19 +8,17 @@ import React from 'react';
  */
 export const urlParameters = (filter) => {
     const param = Object.keys(filter)
-        .filter(key => !['minPrice', 'maxPrice'].includes(key) && filter[key])
-        .map(key => `${key}=${filter[key]}`)
+        .filter((key) => !['minPrice', 'maxPrice'].includes(key) && filter[key])
+        .map((key) => `${key}=${filter[key]}`);
 
-    if (filter.minPrice)
-        param.push(`price[gt]=${filter.minPrice}`);
+    if (filter.minPrice) param.push(`price[gt]=${filter.minPrice}`);
 
-    if (filter.maxPrice)
-        param.push(`price[lt]=${filter.maxPrice}`);
+    if (filter.maxPrice) param.push(`price[lt]=${filter.maxPrice}`);
 
     const allParams = param.join('&');
 
-    return allParams ? `?${allParams}` : '';
-}
+    return allParams ? `?${allParams}` : "";
+};
 
 /**
  * convert EditorJS data to React HTML
@@ -30,20 +28,31 @@ export const urlParameters = (filter) => {
  */
 export const convertDataToHtml = (blocks) => {
     const convertedHtml = blocks.map((block, key) => {
-        if (block.type === "header") {
-            return React.createElement(`h${block.data.level}`, null, block.data.text);
-        } else if (block.type === "paragraph") {
+        if (block.type === 'header') {
+            return React.createElement(
+                `h${block.data.level}`,
+                null,
+                block.data.text
+            );
+        } else if (block.type === 'paragraph') {
             return <p key={key}>{block.data.text}</p>;
-        } else if (block.type === "delimiter") {
+        } else if (block.type === 'delimiter') {
             return <hr key={key} />;
-        } else if (block.type === "image") {
-            return <img key={key} src={block.data.file.url} alt={block.data.caption} />;
-        } else if (block.type === "list") {
-            const listItems = block.data.items.map((item, i) => <li key={i}>{item}</li>);
+        } else if (block.type === 'image') {
+            return (
+                <img
+                    key={key}
+                    src={block.data.file.url}
+                    alt={block.data.caption}
+                />
+            );
+        } else if (block.type === 'list') {
+            const listItems = block.data.items.map((item, i) => (
+                <li key={i}>{item}</li>
+            ));
             return <ul key={key}>{listItems}</ul>;
         }
     });
 
-    return <div className="editor-html">{convertedHtml}</div>;
-}
-
+    return <div className='editor-html'>{convertedHtml}</div>;
+};
