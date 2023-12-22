e3acaf9a5655eb33552cd6d5d600f4fa61eae9f8
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-12-22 17:40:13 +0100

    Feat: Cleanin

--- front/src/layouts/UserLayout.jsx
+++ front/src/layouts/UserLayout.jsx
@@ -13,7 +13,7 @@ const UserLayout = ({children}) => {
     return (
         <>
             <Sidebar content={sidebarContent}/>
-            <main className=" p-6 w-full">
+            <main className="p-6 w-full">
                 {children}
             </main>
         </>
