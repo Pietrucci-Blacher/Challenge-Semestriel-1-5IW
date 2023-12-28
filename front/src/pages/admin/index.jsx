ff3c7f152de8a8e772b292090fa8c24da023ff53
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-12-28 13:34:03 +0100

    Fix conflict for Vercel

--- front/src/pages/admin/index.jsx
+++ front/src/pages/admin/index.jsx
@@ -1,7 +1,3 @@
-// /**
-//  * v0 by Vercel.
-//  * @see https://v0.dev/t/5pCXvVg6JIl
-//  */
 // import Link from "next/link";
 // import { Button } from "@/components/ui/button";
 // import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
@@ -15,151 +11,151 @@
 //   Table,
 // } from "@/components/ui/table";
 //
-// export default function Component() {
-//   return (
-//     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
-//       <aside className="w-64 bg-white dark:bg-gray-800 overflow-auto">
-//         <nav className="flex flex-col p-4 gap-4">
-//           <Link
-//             className="flex items-center gap-2 text-lg font-semibold text-gray-900 dark:text-gray-100"
-//             href="#"
-//           >
-//             <PlaneIcon className="w-6 h-6" />
-//             <span>Dashboard</span>
-//           </Link>
-//           <Button className="rounded-full" size="icon" variant="ghost">
-//             <img
-//               alt="Avatar"
-//               className="rounded-full"
-//               height="32"
-//               src="/placeholder.svg"
-//               style={{
-//                 aspectRatio: "32/32",
-//                 objectFit: "cover",
-//               }}
-//               width="32"
-//             />
-//             <span className="sr-only">Toggle user menu</span>
-//           </Button>
-//         </nav>
-//       </aside>
-//       <main className="flex-1 flex flex-col p-6 gap-6">
-//         <div className="flex justify-between items-center">
-//           <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
-//             Dashboard
-//           </h1>
-//         </div>
-//         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
-//           <Card>
-//             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-//               <CardTitle className="text-sm font-medium">
-//                 Total Bookings
-//               </CardTitle>
-//               <PlaneIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-//             </CardHeader>
-//             <CardContent>
-//               <div className="text-2xl font-bold">1,245</div>
-//               <p className="text-xs text-gray-500 dark:text-gray-400">
-//                 +10% from last month
-//               </p>
-//             </CardContent>
-//           </Card>
-//           <Card>
-//             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-//               <CardTitle className="text-sm font-medium">
-//                 Average Rating
-//               </CardTitle>
-//               <StarIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-//             </CardHeader>
-//             <CardContent>
-//               <div className="text-2xl font-bold">4.2</div>
-//               <p className="text-xs text-gray-500 dark:text-gray-400">
-//                 +0.1 from last month
-//               </p>
-//             </CardContent>
-//           </Card>
-//           <Card>
-//             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-//               <CardTitle className="text-sm font-medium">Revenue</CardTitle>
-//               <DollarSignIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-//             </CardHeader>
-//             <CardContent>
-//               <div className="text-2xl font-bold">$1,234,567</div>
-//               <p className="text-xs text-gray-500 dark:text-gray-400">
-//                 +2% from last month
-//               </p>
-//             </CardContent>
-//           </Card>
-//           <Card>
-//             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-//               <CardTitle className="text-sm font-medium">
-//                 Active Users
-//               </CardTitle>
-//               <GroupIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-//             </CardHeader>
-//             <CardContent>
-//               <div className="text-2xl font-bold">987</div>
-//               <p className="text-xs text-gray-500 dark:text-gray-400">
-//                 +50% from last month
-//               </p>
-//             </CardContent>
-//           </Card>
-//         </div>
-//         <div className="flex flex-col gap-6 md:flex-row md:gap-8">
-//           <Card className="w-full md:w-1/2">
-//             <CardHeader>
-//               <CardTitle className="text-lg font-semibold">
-//                 Booking Trends
-//               </CardTitle>
-//             </CardHeader>
-//             <CardContent>
-//               <CurvedlineChart className="w-full aspect-[4/3]" />
-//             </CardContent>
-//           </Card>
-//           <Card className="w-full md:w-1/2">
-//             <CardHeader>
-//               <CardTitle className="text-lg font-semibold">
-//                 Recent Bookings
-//               </CardTitle>
-//             </CardHeader>
-//             <CardContent>
-//               <Table>
-//                 <TableHeader>
-//                   <TableRow>
-//                     <TableHead className="w-[100px]">Booking ID</TableHead>
-//                     <TableHead>Status</TableHead>
-//                     <TableHead>Price</TableHead>
-//                     <TableHead className="text-right">User</TableHead>
-//                   </TableRow>
-//                 </TableHeader>
-//                 <TableBody>
-//                   <TableRow>
-//                     <TableCell className="font-medium">BK001</TableCell>
-//                     <TableCell>Confirmed</TableCell>
-//                     <TableCell>$250.00</TableCell>
-//                     <TableCell className="text-right">John Doe</TableCell>
-//                   </TableRow>
-//                   <TableRow>
-//                     <TableCell className="font-medium">BK002</TableCell>
-//                     <TableCell>Pending</TableCell>
-//                     <TableCell>$150.00</TableCell>
-//                     <TableCell className="text-right">Jane Smith</TableCell>
-//                   </TableRow>
-//                   <TableRow>
-//                     <TableCell className="font-medium">BK003</TableCell>
-//                     <TableCell>Cancelled</TableCell>
-//                     <TableCell>$350.00</TableCell>
-//                     <TableCell className="text-right">Bob Johnson</TableCell>
-//                   </TableRow>
-//                 </TableBody>
-//               </Table>
-//             </CardContent>
-//           </Card>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
+export default function AdminIndex() {
+    //   return (
+    //     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+    //       <aside className="w-64 bg-white dark:bg-gray-800 overflow-auto">
+    //         <nav className="flex flex-col p-4 gap-4">
+    //           <Link
+    //             className="flex items-center gap-2 text-lg font-semibold text-gray-900 dark:text-gray-100"
+    //             href="#"
+    //           >
+    //             <PlaneIcon className="w-6 h-6" />
+    //             <span>Dashboard</span>
+    //           </Link>
+    //           <Button className="rounded-full" size="icon" variant="ghost">
+    //             <img
+    //               alt="Avatar"
+    //               className="rounded-full"
+    //               height="32"
+    //               src="/placeholder.svg"
+    //               style={{
+    //                 aspectRatio: "32/32",
+    //                 objectFit: "cover",
+    //               }}
+    //               width="32"
+    //             />
+    //             <span className="sr-only">Toggle user menu</span>
+    //           </Button>
+    //         </nav>
+    //       </aside>
+    //       <main className="flex-1 flex flex-col p-6 gap-6">
+    //         <div className="flex justify-between items-center">
+    //           <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
+    //             Dashboard
+    //           </h1>
+    //         </div>
+    //         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+    //           <Card>
+    //             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+    //               <CardTitle className="text-sm font-medium">
+    //                 Total Bookings
+    //               </CardTitle>
+    //               <PlaneIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+    //             </CardHeader>
+    //             <CardContent>
+    //               <div className="text-2xl font-bold">1,245</div>
+    //               <p className="text-xs text-gray-500 dark:text-gray-400">
+    //                 +10% from last month
+    //               </p>
+    //             </CardContent>
+    //           </Card>
+    //           <Card>
+    //             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+    //               <CardTitle className="text-sm font-medium">
+    //                 Average Rating
+    //               </CardTitle>
+    //               <StarIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+    //             </CardHeader>
+    //             <CardContent>
+    //               <div className="text-2xl font-bold">4.2</div>
+    //               <p className="text-xs text-gray-500 dark:text-gray-400">
+    //                 +0.1 from last month
+    //               </p>
+    //             </CardContent>
+    //           </Card>
+    //           <Card>
+    //             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+    //               <CardTitle className="text-sm font-medium">Revenue</CardTitle>
+    //               <DollarSignIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+    //             </CardHeader>
+    //             <CardContent>
+    //               <div className="text-2xl font-bold">$1,234,567</div>
+    //               <p className="text-xs text-gray-500 dark:text-gray-400">
+    //                 +2% from last month
+    //               </p>
+    //             </CardContent>
+    //           </Card>
+    //           <Card>
+    //             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+    //               <CardTitle className="text-sm font-medium">
+    //                 Active Users
+    //               </CardTitle>
+    //               <GroupIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+    //             </CardHeader>
+    //             <CardContent>
+    //               <div className="text-2xl font-bold">987</div>
+    //               <p className="text-xs text-gray-500 dark:text-gray-400">
+    //                 +50% from last month
+    //               </p>
+    //             </CardContent>
+    //           </Card>
+    //         </div>
+    //         <div className="flex flex-col gap-6 md:flex-row md:gap-8">
+    //           <Card className="w-full md:w-1/2">
+    //             <CardHeader>
+    //               <CardTitle className="text-lg font-semibold">
+    //                 Booking Trends
+    //               </CardTitle>
+    //             </CardHeader>
+    //             <CardContent>
+    //               <CurvedlineChart className="w-full aspect-[4/3]" />
+    //             </CardContent>
+    //           </Card>
+    //           <Card className="w-full md:w-1/2">
+    //             <CardHeader>
+    //               <CardTitle className="text-lg font-semibold">
+    //                 Recent Bookings
+    //               </CardTitle>
+    //             </CardHeader>
+    //             <CardContent>
+    //               <Table>
+    //                 <TableHeader>
+    //                   <TableRow>
+    //                     <TableHead className="w-[100px]">Booking ID</TableHead>
+    //                     <TableHead>Status</TableHead>
+    //                     <TableHead>Price</TableHead>
+    //                     <TableHead className="text-right">User</TableHead>
+    //                   </TableRow>
+    //                 </TableHeader>
+    //                 <TableBody>
+    //                   <TableRow>
+    //                     <TableCell className="font-medium">BK001</TableCell>
+    //                     <TableCell>Confirmed</TableCell>
+    //                     <TableCell>$250.00</TableCell>
+    //                     <TableCell className="text-right">John Doe</TableCell>
+    //                   </TableRow>
+    //                   <TableRow>
+    //                     <TableCell className="font-medium">BK002</TableCell>
+    //                     <TableCell>Pending</TableCell>
+    //                     <TableCell>$150.00</TableCell>
+    //                     <TableCell className="text-right">Jane Smith</TableCell>
+    //                   </TableRow>
+    //                   <TableRow>
+    //                     <TableCell className="font-medium">BK003</TableCell>
+    //                     <TableCell>Cancelled</TableCell>
+    //                     <TableCell>$350.00</TableCell>
+    //                     <TableCell className="text-right">Bob Johnson</TableCell>
+    //                   </TableRow>
+    //                 </TableBody>
+    //               </Table>
+    //             </CardContent>
+    //           </Card>
+    //         </div>
+    //       </main>
+    //     </div>
+    //   );
+}
 //
 // function CurvedlineChart(props) {
 //   return (
