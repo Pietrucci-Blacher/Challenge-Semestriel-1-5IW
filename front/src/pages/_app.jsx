c0359095a23e2df6252d4173226b5c5dffaa2df3
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-01-02 18:48:12 +0100

    Fix postCSS message for tailwind and update dashboard

--- front/src/pages/_app.jsx
+++ front/src/pages/_app.jsx
@@ -4,6 +4,11 @@ import { ToastProvider } from '@/providers/ToastProvider';
 import ChooseLayout from '@/components/ChooseLayout';
 import { appWithTranslation } from 'next-i18next';
 import nextI18n from '../../next-i18next.config';
+import { Chart, registerables } from 'chart.js';
+import 'chart.js/auto';
+import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
+
+Chart.register(...registerables);
 
 function MyApp({ Component, pageProps }) {
     return (
