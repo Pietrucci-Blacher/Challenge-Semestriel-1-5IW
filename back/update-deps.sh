9b95d65827aca27e01a3bb970cc577bfd170f702
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-01-18 11:23:16 +0100

    Feat: Update required version

--- back/update-deps.sh
+++ back/update-deps.sh
@@ -11,8 +11,11 @@ docker compose run php /bin/sh -c 'composer update; composer outdated'
 docker compose run pwa /bin/sh -c 'pnpm install; pnpm update; pnpm outdated'
 
 # Update Symfony recipes
+# shellcheck disable=SC2164
 cd api
 composer recipes:update
 
+# shellcheck disable=SC2016
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
+# shellcheck disable=SC2016
 echo 'Run `docker compose up --force-recreate` now and check that everything is fine!'
