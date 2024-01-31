bb68adfabb11347ede7c913c2c3211075e427883
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-01-31 10:03:40 +0100

    Feat: Update some funcs

--- front/src/layouts/DefaultLayout.jsx
+++ front/src/layouts/DefaultLayout.jsx
@@ -1,9 +1,18 @@
+import { useRouter } from 'next/router';
+import PropTypes from "prop-types";
+
 const DefaultLayout = ({ children }) => {
+    const router = useRouter();
+    const isAuthPage = router.pathname === '/auth/login' || router.pathname === '/auth/register';
+    const containerClass = isAuthPage ? 'w-full' : 'p-6 w-full';
+
     return (
-        <>
-            <main className="p-6 w-full">{children}</main>
-        </>
+        <main className={containerClass}>{children}</main>
     );
 };
+
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
 
 export default DefaultLayout;
