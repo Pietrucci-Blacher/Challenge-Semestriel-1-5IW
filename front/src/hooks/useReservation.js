53bb1d152a8b9097e56882139cdccc6ef0a9c878
Ossama9 <oussama.dahbi98@gmail.com>
2024-02-05 22:16:55 +0100

    fix

--- front/src/hooks/useReservation.js
+++ front/src/hooks/useReservation.js
@@ -1,6 +1,6 @@
 import {useCallback, useState} from "react";
 import {
-    addReservation, deleteReservation, deleteReservationService,
+    addReservation, deleteReservationService,
     getEstablishmentReservations, getReservation, getServiceReservations,
     getTeacherReservations,
     getUserReservations, updateReservation
