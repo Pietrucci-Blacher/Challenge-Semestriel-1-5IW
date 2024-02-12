b68a2adab81e68d0585b1dd3a48b28bd95ab7ed8
Ilies <ilies.slim77@gmail.com>
2024-02-12 14:57:39 +0100

    modif map + ajout composant TableEstablishment

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -39,9 +39,10 @@ const MapComponent = () => {
         Promise.all(
             (establishments || []).map(async (establishment) => {
                 try {
+                    const address = `${establishment.street}, ${establishment.city}, ${establishment.zipCode}`;
                     const response = await fetch(
                         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-                            establishment.street,
+                            address
                         )}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`,
                     );
                     const data = await response.json();
@@ -56,9 +57,10 @@ const MapComponent = () => {
                             photo: '/images/immeubles-parisiens-paris-zigzag.jpg',
                             //photo: establishment.photoEstablishment,
                             city: establishment.city,
-                            zipCode: establishment.zipCode,
+                            zipCode: establishment.zipCode
                         };
                     }
+                    console.log("map", response)
                 } catch (error) {
                     console.error(
                         'Erreur lors de la récupération des données de géocodage :',
--- front/src/pages/user/establishment/index.jsx
+++ front/src/pages/user/establishment/index.jsx
@@ -1,70 +1,19 @@
+import { useEffect } from 'react';
 import { useEstablishment } from '@/hooks/useEstablishment';
-import { useEffect, useState } from 'react';
-import { Button as FlowbiteButton, Table } from 'flowbite-react';
-import Link from 'next/link';
-import Spinner from '@/components/Spinner';
 import MapComponent from '@/components/Map';
-
+import EstablishmentTable from '@/components/EstablishmentTable';
 
 export default function ListAllEstablishment() {
     const { establishments, getAllEstablishments } = useEstablishment();
-    const [nombreElements, setNombreElements] = useState(3);
 
     useEffect(() => {
         getAllEstablishments();
     }, [getAllEstablishments]);
 
-    if (!establishments) {
-        return <Spinner />;
-    }
-
-    const renderEstablishments = establishments
-        ? establishments.slice(0, nombreElements).map((establishment) => (
-            <Table.Row key={establishment.id}>
-                <Table.Cell>{establishment.name}</Table.Cell>
-                <Table.Cell>{establishment.street}</Table.Cell>
-                <Table.Cell>{establishment.city}</Table.Cell>
-                <Table.Cell>{establishment.zipCode}</Table.Cell>
-                <Table.Cell>
-                    {establishment.owner.firstname}{' '}
-                    {establishment.owner.lastname}
-                </Table.Cell>
-                <Table.Cell>
-                    <a
-                        className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                        href={`/establishment/${establishment.id}`}
-                    >
-                        Voir
-                    </a>
-                </Table.Cell>
-            </Table.Row>
-        ))
-        : 'Chargement en cours';
-
     return (
         <div>
-
-        <Table hoverable>
-            <Table.Head>
-                <Table.HeadCell>Nom</Table.HeadCell>
-                <Table.HeadCell>Rue</Table.HeadCell>
-                <Table.HeadCell>Ville</Table.HeadCell>
-                <Table.HeadCell>Code postal</Table.HeadCell>
-                <Table.HeadCell>Actions</Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y divide-gray-700">
-                {renderEstablishments}
-            </Table.Body>
-        </Table>
-        {nombreElements < establishments.length && (
-                <FlowbiteButton onClick={() => setNombreElements(nombreElements + 3)}>
-                    Afficher plus
-                </FlowbiteButton>
-            )}
-            <MapComponent></MapComponent>
-
+            <EstablishmentTable establishments={establishments} />
+            <MapComponent />
         </div>
-        
-        
     );
 }
