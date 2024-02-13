d13d4d5ac8d4f3b9d2ed5da9f23f8e6055c6fe6d
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-02-13 09:21:05 +0100

    Feat: Add and fix highest role

--- front/src/hooks/useDatatable.js
+++ front/src/hooks/useDatatable.js
@@ -10,10 +10,40 @@ import { normalize } from '@/utils/data';
 export const useDatatable = () => {
     const [data, setData] = useState([]);
     const [userDetails, setUserDetails] = useState({});
+
+    const getHighestRoleLabel = (roles) => {
+        const roleHierarchy = {
+            ROLE_USER: 1,
+            ROLE_TEACHER: 2,
+            ROLE_PROVIDER: 3,
+            ROLE_ADMIN: 4,
+        };
+
+        const roleLabels = {
+            ROLE_USER: 'Utilisateur',
+            ROLE_TEACHER: 'Enseignant',
+            ROLE_PROVIDER: 'Fournisseur',
+            ROLE_ADMIN: 'Administrateur',
+        };
+
+        const highestRole = roles.reduce((prev, current) => {
+            return roleHierarchy[current] > roleHierarchy[prev]
+                ? current
+                : prev;
+        });
+
+        return roleLabels[highestRole];
+    };
+
     const fetchAllUsersData = useCallback(async () => {
         try {
             const response = await getUsersService();
-            setData(normalize(response));
+            const normalizedData = normalize(response);
+            const dataWithRoleLabels = normalizedData.map((user) => ({
+                ...user,
+                roles: getHighestRoleLabel(user.roles),
+            }));
+            setData(dataWithRoleLabels);
         } catch (error) {
             console.error('Error fetching users:', error);
         }
@@ -22,9 +52,13 @@ export const useDatatable = () => {
     const fetchUserData = useCallback(async (id) => {
         try {
             const response = await getUserService(id);
+            const userWithRoleLabel = {
+                ...response,
+                roles: getHighestRoleLabel(response.roles),
+            };
             setUserDetails((prevDetails) => ({
                 ...prevDetails,
-                [id]: response,
+                [id]: userWithRoleLabel,
             }));
         } catch (error) {
             console.error('Error fetching user:', error);
