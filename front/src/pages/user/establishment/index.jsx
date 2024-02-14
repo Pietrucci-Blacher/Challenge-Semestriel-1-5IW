13c3c66343bf7a40e1aa86d03bf6782ce91b31fc
Ilies <ilies.slim77@gmail.com>
2024-02-14 18:15:08 +0100

    add filter estabmishment

--- front/src/pages/user/establishment/index.jsx
+++ front/src/pages/user/establishment/index.jsx
@@ -1,7 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useEstablishment } from '@/hooks/useEstablishment';
 import MapComponent from '@/components/Map';
 import EstablishmentTable from '@/components/EstablishmentTable';
+import Input from '@/components/Input';
+import { Button, Card, Modal } from 'flowbite-react';
+
+
 
 export default function ListAllEstablishment() {
     const { establishments, getAllEstablishments } = useEstablishment();
@@ -9,6 +13,15 @@ export default function ListAllEstablishment() {
     useEffect(() => {
         getAllEstablishments();
     }, [getAllEstablishments]);
+
+
+    const [formData, setFormData] = useState({
+        title: '',
+    });
+
+    const handleInputSearchChange = (value) => {
+        setFormData({ ...formData, title: value });
+    };
 
     return (
         <div>
