d81573aaf54ddfaae6806e3a41c7f1300af34cca
Ossama9 <oussama.dahbi98@gmail.com>
2024-02-15 11:16:41 +0100

    fix table reservation + service

--- front/src/pages/provider/establishment/index.jsx
+++ front/src/pages/provider/establishment/index.jsx
@@ -1,37 +1,52 @@
-import { useEstablishment } from '@/hooks/useEstablishment';
 import { useContext, useEffect } from 'react';
 import { Button as FlowbiteButton, Table } from 'flowbite-react';
 import Link from 'next/link';
 import { useAuthContext } from '@/providers/AuthProvider';
+import {useEstablishment} from "@/hooks/useEstablishment";
 
 export default function ListEstablishment() {
     const { user } = useAuthContext();
     const { establishments, getMyEstablishments } = useEstablishment();
 
     useEffect(() => {
-        const { id } = user;
-        if (!id) return;
-        getMyEstablishments(id);
+        if (user?.id) {
+            getMyEstablishments(user.id);
+        }
     }, [user, getMyEstablishments]);
 
-    const renderEstablishments = establishments
-        ? establishments.map((establishment) => (
-              <Table.Row key={establishment.id}>
-                  <Table.Cell>{establishment.name}</Table.Cell>
-                  <Table.Cell>{establishment.street}</Table.Cell>
-                  <Table.Cell>{establishment.city}</Table.Cell>
-                  <Table.Cell>{establishment.zipCode}</Table.Cell>
-                  <Table.Cell>
-                      <a
-                          className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                          href={`/provider/establishment/${establishment.id}`}
-                      >
-                          Voir
-                      </a>
-                  </Table.Cell>
-              </Table.Row>
-          ))
-        : 'Chargement en cours';
+    let content;
+    if (establishments === null) {
+        // État de chargement
+        content = (
+            <Table.Row>
+                <Table.Cell colSpan="5">Chargement en cours...</Table.Cell>
+            </Table.Row>
+        );
+    } else if (establishments.length === 0) {
+        // Aucun établissement trouvé
+        content = (
+            <Table.Row>
+                <Table.Cell colSpan="5">Aucun établissement trouvé.</Table.Cell>
+            </Table.Row>
+        );
+    } else {
+        // Affichage des établissements
+        content = establishments.map((establishment) => (
+            <Table.Row key={establishment.id}>
+                <Table.Cell>{establishment.name}</Table.Cell>
+                <Table.Cell>{establishment.street}</Table.Cell>
+                <Table.Cell>{establishment.city}</Table.Cell>
+                <Table.Cell>{establishment.zipCode}</Table.Cell>
+                <Table.Cell>
+                    <Link href={`/provider/establishment/${establishment.id}`} passHref>
+                        <a className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                            Voir
+                        </a>
+                    </Link>
+                </Table.Cell>
+            </Table.Row>
+        ));
+    }
 
     return (
         <>
@@ -43,7 +58,7 @@ export default function ListEstablishment() {
                     <Table.HeadCell>Code postal</Table.HeadCell>
                     <Table.HeadCell>Actions</Table.HeadCell>
                 </Table.Head>
-                <Table.Body>{renderEstablishments}</Table.Body>
+                <Table.Body>{content}</Table.Body>
             </Table>
             <FlowbiteButton
                 className="my-2"
