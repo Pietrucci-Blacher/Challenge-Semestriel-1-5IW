915c0cf2c57c90fb9757d6be700be07063c6ecc5
Ossama9 <78116958+Ossama9@users.noreply.github.com>
2024-02-15 11:31:17 +0100

    Update front/src/pages/user/services/[id].jsx

--- front/src/pages/user/services/[id].jsx
+++ front/src/pages/user/services/[id].jsx
@@ -171,7 +171,6 @@ export default function Id() {
         specialRequest.length > 0
             ? (payload['specialRequest'] = specialRequest)
             : payload;
-        console.log('payload', payload);
         setOpenModal(false);
         await createReservation(payload);
         await getTeacherSchedules(selectedTeacher);
